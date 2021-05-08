b0814ea63676988d29b4c6dccfe9cdd77b16f698
Triips-TheCoder <julian.laballe@mmibordeaux.com>
2021-05-08 13:02:35 +0200

    correction des liens

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root"home#index"
+  root "home#index"
   resources :recipes
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
